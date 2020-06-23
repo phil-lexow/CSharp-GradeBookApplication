b632e067160e18747e2dadc7c6245519c3894cff
BLUEBEAM\plexow <plexow@bluebeam.com>
2020-06-22 18:56:06 -0700

    Completed module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,20 +40,17 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            var type = parts[2];
 
-            if (parts[3] == "standard")
-            {
+            BaseGradeBook gradeBook;
+            if (type == "standard")
                 gradeBook = new StandardGradeBook(name);
-            }
-            else if (parts[3] == "ranked")
-            {
+            if (type == "ranked")
                 gradeBook = new RankedGradeBook(name);
-            }
             else
-            {
-                Console.WriteLine(name + " is not a supported type of gradebook, please try again");
-            }
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again", type);
+                return;
+
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
